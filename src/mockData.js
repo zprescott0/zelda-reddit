2aa2c993c2d4bc4c339d59d77153fafa8b9423db
zprescott0 <76888154+zprescott0@users.noreply.github.com>
2022-09-23 19:18:49 -0400

    Created mock data for practice purposes.

---
+++ src/mockData.js
@@ -0,0 +1,100 @@
+export const mockData_zelda = {
+    posts: {
+        'xl57st': {
+            author: 'itzaspace',
+            created: 1663862132,
+            downs: 0,
+            id: 'xl57st',
+            permalink: '/r/zelda/comments/xl57st/oc_ocarina_of_time_i_did_a_new_animated_wallpaper/',
+            selftext: 'What do you guys think?',
+            thumbnail: 'https://b.thumbs.redditmedia.com/7BSwVTONw9yEhEjdrj8b7Z61bZw9KC94k7QDC9DjyWc.jpg',
+            title: '[OC] Ocarina of Time. I did a new Animated Wallpaper.',
+            ups: 6311
+        },
+        'xlo1lo': {
+            author: 'iamal3x_',
+            created: 1663909237,
+            downs: 0,
+            id: 'xlo1lo',
+            permalink: '/r/zelda/comments/xlo1lo/totk_i_swear_if_they_let_us_use_bombs_while/',
+            selftext: '',
+            thumbnail: 'https://b.thumbs.redditmedia.com/Jug1Xxqtq6OMgCXvzyHSAzR0bse78xL3GyGUgLTBOuk.jpg',
+            title: '[TOTK] i swear if they let us use bombs while flying this thing 🎃👺',
+            ups: 236
+        }
+    }
+};
+
+export const mockData_truezelda = {
+    posts: {
+        'x3bazv': {
+            author: 'ZeldaMod',
+            created: 1662048010,
+            downs: 0,
+            id: 'x3bazv',
+            permalink: '/r/truezelda/comments/x3bazv/general_questions_and_offtopic_discussion_thread/',
+            selftext: 'This thread is for general discussion, from questions or topics that may not merit their own thread to other aspects of life. \n\nExamples include:\n\n* Simple questions or advice that do not necessarily open much room for discussion.\n* Meta conversations, including suggestions for the subreddit themes, contests, or engagement, for examples. Please send specific complaints to the moderators.\n* Topics or prompts unrelated to Legend of Zelda you want to talk about with other members of this subreddit community.\n* Anything else going on, from other games or projects you are enjoying to general slice-of-life topics.',
+            thumbnail: '',
+            title: 'General Questions and Off-topic Discussion Thread',
+            ups: 6
+        },
+        'xkc2kc': {
+            author: 'Princie33',
+            created: 1663783172,
+            downs: 0,
+            id: 'xkc2kc',
+            permalink: '/r/truezelda/comments/xkc2kc/genuine_question_am_i_the_only_one_sick_of_zonai/',
+            selftext: `Like, all the word "Zonai" was was a name given to some ruins in BOTW. It wasn't important, it didn't mean anything. I've seen people compare it to the Sheikah, in an "It's a mysterious tribe we might not learn about until a few games down the line" kinda way, but, like... the Sheikah, y'know, actually existed as characters. The Zonai, not so much. Idk, I just feel like people are overreacting and I think people will be disappointed come May when there's really nothing much about them in the game.`,
+            thumnail: '',
+            title: 'Genuine question, am I the only one sick of Zonai theories?',
+            ups: 78
+        },
+        
+    }
+};
+
+export const mockData_comments = {
+    'xl57st': [{
+        author: 'CleBlackCats',
+        body: 'Incredible mate'
+    }, {
+        author: 'BlueJayTwentyFive',
+        body: 'I hope that ocarina has some seriously good warranty. All joking aside, an absolutely beautiful piece!'
+    }, {
+        author: 'itzaspace',
+        body: "I'm creating some wallpapers in pixel art of Zelda games.\r  \nToday is Ocarina of Time, What other Zelda games would you like to see?  \nDownload the wallpaper versions for mobile and desktop on my Ko-fi:\r  \n[https://ko-fi.com/s/7f8e967d70](https://ko-fi.com/s/7f8e967d70)\r  \n\r  \nThanks for watching!\r  \n[https://twitter.com/1TZAH](https://twitter.com/1TZAH)\r  \n[https://www.instagram.com/1tzah](https://www.instagram.com/1tzah)"
+    }],
+
+    'xlo1lo': [{
+        author: 'Hoodeeee',
+        body: '*Eehh HEH heh Heh heeeh*'
+    }, {
+        author: 'DoctorHanz',
+        body: 'Godspeed spidaman'
+    }, {
+        author: 'Pretzelman718',
+        body: '"You and I are not so different"'
+    }],
+
+    'x3bazv': [{
+        author: 'january-',
+        body: 'Played five hours of Tunic so far, easily the best Zelda game in the past decade.'
+    }, {
+        author: 'Stv13579',
+        body: 'This sub is becoming way too block happy over the most minor things. Because of the new blocking system I had someone earlier who had to message me to reply to my comment because someone upchain decided to block them, and I’ve had to do the same in the past, as I’m sure others have. This is a discussion subreddit, blocking people because you disagree with them is against the spirit of the whole subreddit.'
+    }, {
+        author: 'Maelstrom108',
+        body: 'For those that are of the school of thought that: Light is different to (even if only by quantity or something like that) Twilight, whilst Twilight is most definetly not Darkness;\n\nHow would you categorise Shadow in the specifc instance of Sages of Shadow?'
+    }],
+
+    'xkc2kc': [{
+        author: 'Mishar5k',
+        body: `I think its just that a couple of things we've seen in totk do fit zonai ruins designs. The dragons in the games logo are just like the statues in faron. Creating a champion also points out the zonai spiral pattern which kinda looks like the "magatamas" surrounding the giant figure in the latest trailer. \n\nI will admit that we dont *really* know that zonai will be in the game without any doubt, but for now it's easy to just call them zonai as a placeholder name before we get confirmation.`
+    }, {
+        author: 'Outrageous_Net8365',
+        body: `Zonai &gt; Sheikah tech imo. I\’m glad there\’s a shift of discussion`
+    }, {
+        author: 'easycure',
+        body: "I get where you're coming from, I've felt that way too on occasion, but I'm at the point now where it doesn't bug me and I can take it or leave it. \n\nIf TotK decided to expand on the lore by adding Zonai as some ancient, pre-hylian race, awesome, great, maybe they'll be expanded on in other games too. I don't recall anyone hating on Goron's, Zora, or Ruto as races/tribes that expand the lore and world building a bit better. Zonai could just be that. \n\nIf TotK does nothing with the Zonai other then vaguely allude to them the way they were in BotW, I'm fine with that too. I just want to spend another 150-200hrs exploring every inch of the amazing world Aunuma and his team can craft, and mess around with all the fun new mechanics. \n\nEither way, all the Zelda YouTubers will continue to put out their theories, which can be fun in their own right, even if the theories have no impact on the actual game."
+    }]
+};

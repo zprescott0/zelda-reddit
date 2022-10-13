706806199b229cba8b4720c5e1771b596afdb320
zprescott0 <76888154+zprescott0@users.noreply.github.com>
2022-10-13 10:46:23 -0400

    Created a couple of buttons in App component for testing purposes.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useDispatch, useSelector } from 'react-redux';
+
 import './App.css';
 
 function App() {
+  const handleClick1 = () => {
+    console.log('Testing 1');
+  }
+
+  const handleClick2 = () => {
+    console.log('Testing 2');
+  }
+
+  const handleClick3 = () => {
+    console.log('Testing 3');
+  }
+
   return (
     <div className="App">
-      test
+      <button onClick={handleClick1}>Button 1</button>
+      <button onClick={handleClick2}>Button 2</button>
+      <button onClick={handleClick3}>Button 3</button>
     </div>
   );
 }

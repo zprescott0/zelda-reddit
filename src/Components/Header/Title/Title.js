4515c5a4112c493723b940353417bb9cfa11b6cc
zprescott0 <76888154+zprescott0@users.noreply.github.com>
2022-10-14 12:45:46 -0400

    Created new file, title.css. Imported title.css into Title.js.

--- src/Components/Header/Title/Title.js
+++ src/Components/Header/Title/Title.js
@@ -1,12 +1,16 @@
 import triforceLogo from '../../../Images/assets/triforceLogo.png';
+import './title.css';
 
 export const Title = props => {
     return (
-        <span className='title'>
-            Zelda
-            <img src={triforceLogo}
-            className='logo' />
-            Reddit
-        </span>
+        <div className='title-container'>
+            <span className='title'>
+                <span className='title-text'>Zelda</span>
+                <img src={triforceLogo}
+                className='logo'
+                alt='triforce logo' />
+                <span className='title-text'>Reddit</span>
+            </span>
+        </div>
     );
 };

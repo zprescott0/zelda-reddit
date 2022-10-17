b8a5ada059b8a6ae4af88d7c81af7bb12760472a
zprescott0 <76888154+zprescott0@users.noreply.github.com>
2022-10-17 08:14:22 -0400

    Set up SearchBar component to use Redux.

--- src/Components/Header/SearchBar/SearchBar.js
+++ src/Components/Header/SearchBar/SearchBar.js
@@ -1,13 +1,27 @@
+import { useSelector, useDispatch } from 'react-redux';
+
+import { selectSearchTerm, setSearchTerm } from '../../../store/searchTermSlice';
+
 import './searchbar.css';
 import magnifyingGlass from '../../../Images/assets/magnifyingGlass.png';
 
 export const SearchBar = props => {
+    const dispatch = useDispatch();
+    const searchTerm = useSelector(selectSearchTerm);
+
+    const onInputChange = event => {
+        event.preventDefault();
+        const inputValue = event.target.value;
+        
+        dispatch(setSearchTerm(inputValue));
+    }
 
     return (
         <div className='searchbar-container'>
             <div className='search-bar'>
-                <input type='text' id='search-bar-input' className='search-bar-input' />
-                <label for='search-bar-input'>
+                <input type='text' id='search-bar-input' className='search-bar-input'
+                onChange={onInputChange} value={searchTerm}/>
+                <label htmlFor='search-bar-input'>
                     <img src={magnifyingGlass} alt='magnifying glass' 
                     id='search-bar-icon' />
                 </label>

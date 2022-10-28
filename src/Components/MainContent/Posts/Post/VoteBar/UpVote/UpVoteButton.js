146d2c96b6636cf28cab985e680242ba3bb379d9
zprescott0 <76888154+zprescott0@users.noreply.github.com>
2022-10-28 19:44:09 -0400

    Imported necessary images.

--- src/Components/MainContent/Posts/Post/VoteBar/UpVote/UpVoteButton.js
+++ src/Components/MainContent/Posts/Post/VoteBar/UpVote/UpVoteButton.js
@@ -1,6 +1,12 @@
+import upArrow from '../../../../../../Images/assets/upArrow.png';
+import upArrowClicked from '../../../../../../Images/assets/upArrow-Clicked.png';
+import upArrowHover from '../../../../../../Images/assets/upArrowHover.png';
+
 export const UpVoteButton = props => {
 
     return <div className='upvote-button-container'>
-        <span>XXXXX</span>
+        <img src={upArrow}
+        className='upvote-button'
+        alt='Upvote button' />
     </div>;
 };

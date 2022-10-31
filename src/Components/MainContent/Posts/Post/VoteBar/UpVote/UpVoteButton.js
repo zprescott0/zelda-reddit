ba1caf8add4efb0791bbc2f06fee1ba1ba5fdb8a
zprescott0 <76888154+zprescott0@users.noreply.github.com>
2022-10-31 08:46:22 -0400

    Created UpVoteButton component.

--- src/Components/MainContent/Posts/Post/VoteBar/UpVote/UpVoteButton.js
+++ src/Components/MainContent/Posts/Post/VoteBar/UpVote/UpVoteButton.js
@@ -1,12 +1,34 @@
+import { useState } from 'react';
+
 import upArrow from '../../../../../../Images/assets/upArrow.png';
 import upArrowClicked from '../../../../../../Images/assets/upArrow-Clicked.png';
 import upArrowHover from '../../../../../../Images/assets/upArrowHover.png';
 
 export const UpVoteButton = props => {
 
+    const [image, setImage] = useState(upArrow);
+
+    const handleMouseEnter = (e) => {
+        e.preventDefault();
+        setImage(prevState => prevState === upArrowClicked ? upArrowClicked : upArrowHover);
+    }
+
+    const handleMouseOut = (e) => {
+        e.preventDefault();
+        setImage(prevState => prevState === upArrowClicked ? upArrowClicked : upArrow);
+    }
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        setImage(prevState => prevState === upArrowClicked ? upArrowHover : upArrowClicked);
+    }
+
     return <div className='upvote-button-container'>
-        <img src={upArrow}
+        <img src={image}
         className='upvote-button'
-        alt='Upvote button' />
+        alt='Upvote button'
+        onMouseEnter={handleMouseEnter}
+        onMouseOut={handleMouseOut}
+        onClick={handleClick} />
     </div>;
 };

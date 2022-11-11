b635c500ed3d8564b1970e5428f61e37ae53fbb5
zprescott0 <76888154+zprescott0@users.noreply.github.com>
2022-11-10 20:54:13 -0500

    Made VoteBar component position itself and its components correctly.

--- src/Components/MainContent/Posts/Post/VoteBar/VoteBar.js
+++ src/Components/MainContent/Posts/Post/VoteBar/VoteBar.js
@@ -1,3 +1,5 @@
+import './VoteBar.css';
+
 import { UpVoteButton } from "./UpVote/UpVoteButton";
 import { DownVoteButton } from "./DownVote/DownVoteButton";
 import { Score } from "./Score/Score";
@@ -5,9 +7,13 @@ import { Score } from "./Score/Score";
 export const VoteBar = props => {
 
     return <div className='votebar'>
-        <UpVoteButton />
-        <Score score={56} />
-        <Score score={25} />
-        <DownVoteButton />
+        <div className='upvote vote-container'>
+            <UpVoteButton />
+            <Score score={56} />
+        </div>
+        <div className='downvote vote-container'>
+            <Score score={25} />
+            <DownVoteButton />
+        </div>
     </div>;
 };
